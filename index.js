5f7534cdb37c19257247b460322d24f277329300
Atul Kumar <atul48649@gmail.com>
2022-01-19 16:54:02 +0530

    create signup and signin pages and added signup

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const { path } = require('express/lib/application');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+
+app.use(express.urlencoded());
+
+app.use(cookieParser());
 
 // use static files
 app.use(express.static('./assets'));
@@ -16,7 +21,7 @@ app.set('layout extractScripts', true);
 // use express router
 app.use('/', require('./routes/index')); // here we can also write as '(./routes)' as it directly fetches index.js
 
-// set up the view engine
+// setting up template engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
