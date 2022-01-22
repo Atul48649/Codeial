4b70c6abd86fcbc7a96535b195df47892c91827e
Atul Kumar <atul48649@gmail.com>
2022-01-22 22:38:10 +0530

    add flash messages

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -14,10 +14,12 @@ module.exports.create = async function (req, res) {
 
             post.comments.push(comment);
             post.save();
+            req.flash('success', 'Comment published!')
             res.redirect('/');
         }
     } catch (err) {
-        console.log('Error ', err);
+        req.flash('error', err);
+        return res.redirect('back');
     }
 }
 
@@ -28,11 +30,14 @@ module.exports.destroy = async function (req, res) {
             let postId = comment.post;
             comment.remove();
             let post = await Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id } });
+            req.flash('success', 'Comment deleted!');
             return res.redirect('back');
         } else {
+            req.flash('error', 'Unauthorized');
             return res.redirect('back');
         }
     } catch (err) {
-        console.log('Error ', err);
+        req.flash('error', err);
+        return res.redirect('back');
     }
 }
